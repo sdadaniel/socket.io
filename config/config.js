529f9e6a4c2f539f3995050a6dde60b342ac851f
sdadaniel <54664264+sdadaniel@users.noreply.github.com>
2021-05-31 18:17:16 +0900

    3

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 module.exports =
   {
     "development": {
-      "username": "root",
+      "username": process.env.user,
       "password": process.env.SEQUELIZE_PASSWORD,
       "database": "database_development",
       "host": "127.0.0.1",
